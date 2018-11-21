184ae49a387bf48ba102ef40afbae58eaf6edc3d
Kotori316 <Kotori316@gmail.com>
2018-11-21 16:33:30 +0900

    Separating logic

--- scala/com/kotori316/transformer/block/TileSource.scala
+++ scala/com/kotori316/transformer/block/TileSource.scala
@@ -8,12 +8,30 @@ import net.minecraftforge.energy.{CapabilityEnergy, IEnergyStorage}
 class TileSource extends TileEntity with ITickable {
     var amount = 1000
 
+    var f = (_: Long) => 1
+
+    var T = 1
+
+    val energyStorage = new IEnergyStorage {
+        override def receiveEnergy(maxReceive: Int, simulate: Boolean): Int = 0
+
+        override def extractEnergy(maxExtract: Int, simulate: Boolean): Int = amount
+
+        override def getEnergyStored: Int = amount
+
+        override def getMaxEnergyStored: Int = amount
+
+        override def canExtract: Boolean = true
+
+        override def canReceive: Boolean = false
+    }
+
     override def update(): Unit = {
         if (world.isRemote) return
-        for (f <- EnumFacing.VALUES;
-             tile <- Option(world.getTileEntity(pos.offset(f)));
-             handler <- Option(tile.getCapability(CapabilityEnergy.ENERGY, f.getOpposite))) {
-            val i = handler.receiveEnergy(amount, false)
+        for (facing <- EnumFacing.VALUES;
+             tile <- Option(world.getTileEntity(pos.offset(facing)));
+             handler <- Option(tile.getCapability(CapabilityEnergy.ENERGY, facing.getOpposite))) {
+            val i = handler.receiveEnergy(amount * f(world.getTotalWorldTime / T), false)
             if (i > 0) {
                 handler.receiveEnergy(i, true)
             }
@@ -26,19 +44,7 @@ class TileSource extends TileEntity with ITickable {
 
     override def getCapability[T](capability: Capability[T], facing: EnumFacing): T = {
         if (capability == CapabilityEnergy.ENERGY) {
-            CapabilityEnergy.ENERGY.cast(new IEnergyStorage {
-                override def receiveEnergy(maxReceive: Int, simulate: Boolean): Int = 0
-
-                override def extractEnergy(maxExtract: Int, simulate: Boolean): Int = amount
-
-                override def getEnergyStored: Int = amount
-
-                override def getMaxEnergyStored: Int = amount
-
-                override def canExtract: Boolean = true
-
-                override def canReceive: Boolean = false
-            })
+            CapabilityEnergy.ENERGY.cast(energyStorage)
         } else
             super.getCapability(capability, facing)
     }
