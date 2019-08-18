3c4ae2faf72c8044be86a39fc2a27807ce8fb458
Kotori316 <Kotori316@gmail.com>
2019-08-18 23:30:16 +0900

    Output energy to adjacent tiles

--- src/main/java/com/kotori316/transformer/TileSource.java
+++ src/main/java/com/kotori316/transformer/TileSource.java
@@ -1,11 +1,14 @@
 package com.kotori316.transformer;
 
+import java.util.stream.Stream;
+
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.play.server.SPacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.ITickable;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.capabilities.Capability;
@@ -16,7 +19,7 @@ import net.minecraftforge.energy.IEnergyStorage;
 import com.kotori316.transformer.packets.EnergyUpdateMessage;
 import com.kotori316.transformer.packets.PacketHandler;
 
-public class TileSource extends TileEntity {
+public class TileSource extends TileEntity implements ITickable {
 
     public static final int INITIAL_CAPACITY = 1000;
     public static final String GUI_ID = EnergyTransformer.modID + ":gui_" + BlockSource.NAME;
@@ -71,6 +74,20 @@ public class TileSource extends TileEntity {
         storage = new Storage(newCapacity);
         if (hasWorld() && world.isRemote) {
             PacketHandler.sendToServer(new EnergyUpdateMessage(this, newCapacity));
+        }
+    }
+
+    @Override
+    public void tick() {
+        if (world != null && !world.isRemote) {
+            Stream.of(EnumFacing.values())
+                .forEach(enumFacing -> {
+                    TileEntity entity = world.getTileEntity(pos.offset(enumFacing));
+                    if (entity != null) {
+                        entity.getCapability(CapabilityEnergy.ENERGY, enumFacing.getOpposite()).
+                            ifPresent(e -> e.receiveEnergy(storage.getMaxEnergyStored(), false));
+                    }
+                });
         }
     }
 
